2a6c1fdf15b17f4b13b0490612214f73a6b50fdb
RoyMoyal <144587952+RoyMoyal@users.noreply.github.com>
2024-05-10 18:31:28 +0300

    Shiur2.java

--- Shiur2.java
+++ Shiur2.java
@@ -1,1 +1,48 @@
-//1.
+public class Tester 
+{
+  public static void main(String[] args) 
+	{
+	 //Exercise 1
+	 int l=5 , k=10 , z=15;
+	 System.out.println(l);
+	 System.out.println(k);
+	 System.out.println(z);
+	 
+	 //Exercise 2
+	 float g=4;
+	 int h=5;
+	 System.out.println(h+"   "+g);
+	 
+	 //Exercise 3
+	 char c='R';
+	 char v='o';
+	 char p='y';
+	 System.out.println(c+v+p);
+	 
+	 //Exercise 4
+	 int e=3 , q=4;
+	 System.out.println(e+" is bigger than "+q);
+	 
+	 //Exercise 5
+	 int grade1=80 , grade2=90 , grade3=85;
+	 float average=(float)(grade1+grade2+grade3)/3;
+	 System.out.println("The average is " + average);
+	 
+	 //Exercise 6
+	 int a=5 ,b=10;
+	 float yter=(a*a)+(b*b);
+	 System.out.println("the yter is " + yter);
+	 
+	 //Exercise 7
+	 int x=5;
+	 int y=(x*2)+5;
+     System.out.println("the value of y is "+y);
+     
+     //Exercise 8
+     int hours=3;
+     String sentense1="I like to study computer sciense";
+	 String sentense2=" and the refore ready to invest ";
+	 String sentense3=" hours a day for practice.";
+     System.out.print(sentense1+sentense2+hours+sentense3);
+	}
+}
